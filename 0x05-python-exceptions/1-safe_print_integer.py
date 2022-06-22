ab6fa17b2956788fe2af30306c9ab4fb054924ff
root <awemary97@gmail.com>
2022-06-22 03:50:46 -0700

    error and exception handling

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-        try:
-                    print("{:d}".format(value))
-                            result = True
-                                except:
-                                            result = False
-                                                finally:
-                                                            return(result)
+    try:
+        if isinstance(value, int):
+            print("{:d}".format(value))
+            return True
+        else:
+            return False
+    except (TypeError, ValueError):
+        print("", end="")
